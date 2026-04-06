f33db1db9431d9cbf38927b3d85de7102983a976
Gaidi <46573819+arnisto@users.noreply.github.com>
2026-04-06 14:00:55 +0200

    Time: 53 ms (39.63%), Space: 54.5 MB (25.09%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+   
+    return async function(...args) {
+        return new Promise((resolve,reject)=>{
+           const time = setTimeout(()=>{
+            reject("Time Limit Exceeded")
+           },t);
+           fn(...args).then((res)=>{
+            clearTimeout(time);
+            resolve(res);
+
+           }).catch((err)=>{
+            clearTimeout(time)
+            reject(err)
+           })
+
+        })
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
